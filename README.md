b5e077bbad56ba3a2525435f8caff0d3c70b6d8e
kurborg <kurborg@gmail.com>
2024-10-01 22:04:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # eweek_arduino
+
+## Dependencies
+- [Arduino IDE 2.3.3](https://www.arduino.cc/en/software)
+
+## Resources
+- [QTR-8RC Reflectance Sensor Array](https://www.pololu.com/product/961)
+- [Arduino UNO R3 Documentation](https://docs.arduino.cc/hardware/uno-rev3/)
+
+## Device Startup
+1. Plug the arduino into your laptop and ensure the power LED comes on
+1. Open Windows Device Manager, look to see what COM port the arduino uno appears on
+
+    !["Image description"](.img/devices.png)
+
+1. In arduino IDE, select the board option of Arduino Uno then select the correct COM port
+
+    !["Image description"](.img/ide_board.png)
+
+1. If you hit the upload (arrow button) on a basic file with an empty setup{} and loop{} you should see the 2 LED's labeled TX and RX flash rapidly indicating that a connection is made to the host and data is being transferred
+
+
+## Examples
+1. In the Arduino IDE, examples can be found under File->Examples
+1. Open a basic blink example in File->Examples->0.1Basics->Blink
+1. Once it's open and you're connected to the Arduino Uno then hit the upload (arrow) button to run and the designated LED near digital pin 13 should blink, while the TX and RX pins also blink rapidly to show that data is being transferred from host
---
+++ src/src.ino
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+
+const int sensorPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+enum Position {
+  LEFT,
+  RIGHT,
+  CENTER
+};
+
+enum Speed {
+  SLOW,
+  MEDIUM, 
+  FAST
+};
+
+void setLeftMotorSpeed(Speed speed) {
+  // Implement your logic to control the left motor's speed
+}
+void setRightMotorSpeed(Speed speed) {
+  // Implement your logic to control the right motor's speed
+}
+
+Position determinePosition(int sensorValues) {
+  // Implement your logic here to determine the position based on the sensor values
+  // For example, you might check if the leftmost or rightmost sensors are activated
+  // to determine if the robot is too far left or too far right.
+}
+
+int readSensors() {
+  int sensorValues = 0;
+  for (int i = 0; i < 8; i++) {
+    sensorValues |= digitalRead(sensorPins[i]) << i;
+  }
+  return sensorValues;
+}
+
+void setup() {
+  for (int i = 0; i < 8; i++) {
+    pinMode(sensorPins[i], INPUT);
+  }
+}
+
+void loop() {
+  int sensorValues = readSensors();
+  int position = determinePosition(sensorValues);
+
+  // Adjust motor speeds based on the position
+  if (position == LEFT) {
+    setLeftMotorSpeed(SLOW);
+    setRightMotorSpeed(FAST);
+  } else if (position == RIGHT) {
+    setLeftMotorSpeed(FAST);
+    setRightMotorSpeed(SLOW);
+  } else {
+    setLeftMotorSpeed(MEDIUM);
+    setRightMotorSpeed(MEDIUM);
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+
+// PSEUDO CODE for basic PID implementation
+
+// double Kp = 0.5; 
+// double Ki = 0.1;
+// double Kd = 0.2;
+
+// double error = 0;
+// double previousError = 0;
+// double integral = 0;
+// double derivative = 0;
+// double pidOutput = 0;
+
+// void loop() {
+//   // ... (rest of your code)
+
+//   // Calculate PID output
+//   error = desiredPosition - position;
+//   integral += error;
+//   derivative = (error - previousError);
+//   pidOutput = Kp * error + Ki * integral + Kd * derivative;
+
+//   // Adjust motor speeds
+//   setLeftMotorSpeed(baseSpeed + pidOutput);
+//   setRightMotorSpeed(baseSpeed - pidOutput);
+
+//   // Update previous error
+//   previousError = error;
+// }
